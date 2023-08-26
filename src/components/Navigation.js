7e439eabbad73edbb0402ab57a51bcc2b63ecd39
Deveroonie <sirepiclydev@gmail.com>
2023-08-26 16:43:39 +0100

    final fix i hope

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ export default function Navigation() {
     return (
       <header className="text-gray-600 body-font">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a className="flex title-font font-medium items-center  mb-4 md:mb-0">
+      <a className="flex title-font font-medium items-center  mb-4 md:mb-0" href="/">
         <span className="ml-3 text-xl text-white nav-header">Deveroonie</span>
       </a>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
