b47eec0f4cea4eb2aa084b8829da3c0fb18c4fe4
Deveroonie <sirepiclydev@gmail.com>
2023-08-26 16:38:28 +0100

    fix some warns

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -16,24 +15,6 @@ function App() {
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </Router>
-  );
-}
-
-//function Home() {
-//  return (
-//    <div style={{ padding: 20 }}>
-//      <h2>Home View</h2>
-//      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-//    </div>
-//  );
-//}
-
-function About() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>About View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
   );
 }
 
