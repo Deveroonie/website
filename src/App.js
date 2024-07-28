632549ff1829d8d603fb151e61dd6cf779ca5478
Deveroonie <me@deveroonie.uk>
2024-07-28 20:59:00 +0100

    chore: ⬇️Downgrade web-vitals to 4.2.1 (to test dependabot commit message) ⬇

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
+import NotFound from "./pages/404";
 
 
 function App() {
@@ -12,19 +13,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="*" element={<NoMatch />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
 }
 
-function NoMatch() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>404: Page Not Found</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-    </div>
-  );
-};
 
 export default App;
