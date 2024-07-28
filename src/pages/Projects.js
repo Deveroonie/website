6f338d7da5ec717615253dc20478ec1243dd41f3
Deveroonie <me@deveroonie.uk>
2024-07-28 20:30:31 +0100

    fix: 🐛💩 Remove HTML/CSS/JS references in projects, simplify cards. 💩🐛

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -31,18 +31,14 @@ function Projects() {
     <h3
         className="text-right hfont text-3xl">
             <a href="https://tflstatus.deveroonie.uk/" target="_blank" rel="noreferrer">TfL Status Website</a>&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faHtml5} />&nbsp;
-            <FontAwesomeIcon icon={faCss3Alt} />&nbsp;
-            <FontAwesomeIcon icon={faSquareJs} />&nbsp;
             <FontAwesomeIcon icon={faReact} />
         </h3>
         <div className="flx">
         <img src="/TFL.png" width={192} height={192} className="proj-icon" alt="TFL's Logo"></img>
         <br />
         <p className="text-right">
-            A website to see the status of the TfL network (Underground, Elizabeth, DLR, Tramlink, and ferries!)<br />
-            It uses HTML/CSS, as well as TailwindCSS, React and Axios-HTTP.<br />For the data I used <a href="https://api.tfl.gov.uk/" target="_blank" rel="noreferrer">TfL's Unified API</a>.<br />
-            You can see the source code <a href="https://github.com/Deveroonie/TfLStatus" target="_blank" rel="noreferrer">here</a>.
+            A website to see the status of the TfL network. It uses TailwindCSS<br /> and React.
+            You can see the source code <a href="https://github.com/Deveroonie/TfLStatus" target="_blank" rel="noreferrer" className="underline">here</a>.
         </p>  
         </div>
 
@@ -52,19 +48,15 @@ function Projects() {
     <h3
         className="text-right hfont text-3xl">
             <a href="https://nrstatus.deveroonie.uk/" target="_blank" rel="noreferrer">National Rail Status Website</a>&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faHtml5} />&nbsp;
-            <FontAwesomeIcon icon={faCss3Alt} />&nbsp;
-            <FontAwesomeIcon icon={faSquareJs} />&nbsp;
             <FontAwesomeIcon icon={faReact} />
         </h3>
         <div className="flx">
         <img src="/NR.png" width={192} height={192} className="proj-icon" alt="National Rail's Logo"></img>
         <br />
         <p className="text-right">
-            A website to see the status of the National Rail network (Southern Rail, ThamesLink, etc)<br />
-            It uses HTML/CSS, as well as TailwindCSS, React and Axios-HTTP.<br />
-            For the data, I used <a href="https://api.tfl.gov.uk" target="_blank" rel="noreferrer">TfL's Unified API</a>.<br />
-            You can see the source code <a href="https://github.com/Deveroonie/NRStatus" target="_blank" rel="noreferrer">here</a>.
+            A website to see the status of the National Rail network.
+            It uses TailwindCSS, and React.<br />
+            You can see the source code <a href="https://github.com/Deveroonie/NRStatus" target="_blank" rel="noreferrer" class="underline">here</a>.
         </p>  
         </div>
     </div>
@@ -72,18 +64,15 @@ function Projects() {
     <h3
         className="text-right hfont text-3xl">
             <a href="https://nrdepartures.deveroonie.uk/" target="_blank" rel="noreferrer">National Rail Departures Website</a>&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faHtml5} />&nbsp;
-            <FontAwesomeIcon icon={faCss3Alt} />&nbsp;
-            <FontAwesomeIcon icon={faSquareJs} />&nbsp;
             <FontAwesomeIcon icon={faReact} />
         </h3>
         <div className="flx">
         <img src="/NR.png" width={192} height={192} className="proj-icon" alt="National Rail's Log"></img>
         <br />
         <p className="text-right">
-            A website to see the live departures/arrivals of any station on the National Rail network<br />
-            It uses HTML/CSS, as well as TailwindCSS, React and Axios-HTTP.<br />For the data I used <a href="https://huxley2.azurewebsites.net" target="_blank" rel="noreferrer">Huxley 2</a>.<br />
-            You can see the source code <a href="https://github.com/Deveroonie/NRDepartures" target="_blank" rel="noreferrer">here</a>.
+            A website to see the live departures/arrivals of any station on the National Rail network.
+            It uses TailwindCSS, and React.
+            You can see the source code <a href="https://github.com/Deveroonie/NRDepartures" target="_blank" rel="noreferrer" className="underline">here</a>.
         </p> 
         </div>
     </div>
@@ -101,8 +90,8 @@ function Projects() {
         <br />
         <p className="text-right">
             This website. It displays information about the frameworks that I can work with, and the projects that I have made.<br />
-            It uses HTML/CSS/JS, as well as React and TailwindCSS.<br />
-            You can see the source code <a href="https://github.com/Deveroonie/Website" target="_blank" rel="noreferrer">here</a>.
+            It uses React and TailwindCSS.<br />
+            You can see the source code <a href="https://github.com/Deveroonie/Website" target="_blank" rel="noreferrer" className='underline'>here</a>.
         </p>  
         </div>
     </div>
