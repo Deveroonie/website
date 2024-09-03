af73c7d1392ac5dc7558529488968e65b81af3c3
Deveroonie <sirepiclydev@gmail.com>
2024-09-03 10:16:35 +0100

    fix: all text on navbar now uses <Link> for faster loading

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,9 +3,9 @@ export default function Navigation() {
     return (
       <header className="text-gray-600 body-font">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a className="flex title-font font-medium items-center  mb-4 md:mb-0" href="/">
+      <Link className="flex title-font font-medium items-center  mb-4 md:mb-0" to="/">
         <span className="ml-3 text-xl text-white nav-header sm:text-center">Deveroonie</span>
-      </a>
+      </Link>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <Link className="mr-5 hover:text-violet-400 text-white" to="/">Home Page</Link>
         <Link className="mr-5 hover:text-violet-400 text-white" to="/experience">Experience</Link>
